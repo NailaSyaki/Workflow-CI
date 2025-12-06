432847749d66416959b964575a3d95ce3614a5a0
Naila Syaki <165618505+NailaSyaki@users.noreply.github.com>
2025-12-06 23:54:21 +0700

    Refactor modelling.py to improve imports and logging

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,8 +1,8 @@
 import mlflow
 import mlflow.sklearn
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-import pandas as pd
 
 # Load dataset
 df = pd.read_csv("katalog-gempa_preprocessing.csv")
@@ -16,15 +16,12 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Aktifkan autolog MLflow
+mlflow.end_run()
 mlflow.autolog()
 
-# Training model
 with mlflow.start_run():
-
     model = RandomForestRegressor()
     model.fit(X_train, y_train)
 
     mlflow.sklearn.log_model(model, artifact_path="model")
-
-    print("Training selesai. Model logged ke artifacts/model")
+    print("Training selesai. Model logged di artifacts/model")
