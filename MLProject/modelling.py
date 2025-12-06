fec0f127468c9cf01fbc6ebba8f5c9587936634e
Naila Syaki <165618505+NailaSyaki@users.noreply.github.com>
2025-12-07 00:04:25 +0700

    Update dataset loading and MLflow run management

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,30 +1,29 @@
 import mlflow
 import mlflow.sklearn
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-import pandas as pd
 
+# Tracking lokal
 mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("gempa-rf-experiment")
 
-# Load dataset (hasil preprocessing)
+mlflow.end_run()
+
+# Load dataset
 df = pd.read_csv("katalog-gempa_preprocessing.csv")
 
-# Tentukan X dan y
 X = df.drop(columns=["mag"])
 y = df["mag"]
 
-# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-mlflow.end_run()
 mlflow.autolog()
 
-with mlflow.start_run():
+with mlflow.start_run(nested=True):
     model = RandomForestRegressor()
     model.fit(X_train, y_train)
 
     mlflow.sklearn.log_model(model, artifact_path="model")
-    print("Training selesai. Model tersimpan di mlruns/<exp>/<run>/artifacts/model")
