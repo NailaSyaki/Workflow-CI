fce9e91d736ae31fe6061e69a333349b38085d13
Naila Syaki <165618505+NailaSyaki@users.noreply.github.com>
2025-12-06 23:16:50 +0700

    Refine comments and update model logging message
    Updated comments for clarity and changed logging message.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,21 +4,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
 
-# Load dataset
+# Load dataset (hasil preprocessing)
 df = pd.read_csv("katalog-gempa_preprocessing.csv")
 
-X = df.drop(columns=["mag"])
+# Tentukan X dan y (ganti sesuai datasetmu)
+X = df.drop(columns=["mag"]) 
 y = df["mag"]
 
+# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# Aktifkan autolog
+mlflow.autolog()
+
+# Training model
 with mlflow.start_run():
 
     model = RandomForestRegressor()
     model.fit(X_train, y_train)
 
-    mlflow.sklearn.log_model(model, artifact_path="model")
-
-    print("Training selesai. Model tersimpan di artifacts/model.")
+    print("Training selesai. Artifacts tersimpan di MLflow.")
