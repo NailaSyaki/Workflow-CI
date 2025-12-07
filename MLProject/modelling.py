c208a7bbf7947a15f217aac6a9276ed770628f1b
Naila Syaki <165618505+NailaSyaki@users.noreply.github.com>
2025-12-07 13:13:58 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,11 +6,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
-# Setup Argument Parser
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path", type=str, required=True)
 parser.add_argument("--run_name", type=str, default="Model Random Forest")
 args = parser.parse_args()
+
+print("Dataset:", args.data_path)
 
 df = pd.read_csv(args.data_path)
 X = df.drop(columns=["mag"])
@@ -20,18 +21,17 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Start MLflow run
-with mlflow.start_run(run_name=args.run_name):
-    mlflow.sklearn.autolog()
+# MLProject sudah meng-handle run, jadi TIDAK boleh pakai start_run()
+mlflow.sklearn.autolog()
 
-    model = RandomForestRegressor()
-    model.fit(X_train, y_train)
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
 
-    preds = model.predict(X_test)
-    mae = mean_absolute_error(y_test, preds)
-    mlflow.log_metric("mae_manual", mae)
+preds = model.predict(X_test)
+mae = mean_absolute_error(y_test, preds)
+mlflow.log_metric("mae_manual", mae)
 
-    print("MAE:", mae)
+print("MAE:", mae)
 
-    print("Saving model artifact to 'model' directory...")
-    mlflow.sklearn.log_model(model, "model")
+print("Saving model artifact to 'model' directory...")
+mlflow.sklearn.log_model(model, "model")
