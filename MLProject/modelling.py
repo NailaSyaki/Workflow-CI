d2c34461c771b6c98a5d83cdcb90607c5b7789e7
Naila Syaki <165618505+NailaSyaki@users.noreply.github.com>
2025-12-07 10:36:35 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,16 +3,15 @@ import mlflow.sklearn
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 
-# Set tracking directory
 mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("gempa-experiment")
 
 df = pd.read_csv("katalog-gempa_preprocessing.csv")
 
-# Pisahkan fitur & target
-X = df.drop(columns=["mag"])  
+# Pisahkan fitur dan target
+X = df.drop(columns=["mag"])
 y = df["mag"]
 
 # Train-test split
@@ -20,14 +19,16 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+mlflow.autolog()
+
+# Mulai MLflow run
 with mlflow.start_run():
     model = RandomForestRegressor()
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
-    mse = mean_squared_error(y_test, preds)
+    mae = mean_absolute_error(y_test, preds)
 
-    print("MSE:", mse)
-
-    mlflow.log_metric("mse", mse)
     mlflow.sklearn.log_model(model, "model")
+
+    print(f"MAE: {mae}")
