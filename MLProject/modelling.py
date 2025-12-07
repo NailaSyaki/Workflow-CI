7b8169f8e9f3624079ae9a6941dc060ea455537f
Naila Syaki <165618505+NailaSyaki@users.noreply.github.com>
2025-12-07 11:05:26 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,28 +1,32 @@
+import os
 import mlflow
 import mlflow.sklearn
 import pandas as pd
-import os
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("gempa-experiment")
-
+# ========== Path Dataset ==========
 base_path = os.path.dirname(os.path.abspath(__file__))
 csv_path = os.path.join(base_path, "katalog-gempa_preprocessing.csv")
 
+print("📌 Loading dataset from:", csv_path)
 df = pd.read_csv(csv_path)
 
-# Pisahkan fitur dan target
+# ========== Pisahkan fitur dan target ==========
 X = df.drop(columns=["mag"])
 y = df["mag"]
 
-# Train-test split
+# ========== Train-test split ==========
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
+tracking_uri = "file:" + os.path.join(base_path, "mlruns")
+print("📌 Tracking URI:", tracking_uri)
+
+mlflow.set_tracking_uri(tracking_uri)
+mlflow.set_experiment("gempa-experiment")
 
 mlflow.autolog()
 
@@ -33,7 +37,6 @@ with mlflow.start_run():
     preds = model.predict(X_test)
     mae = mean_absolute_error(y_test, preds)
 
-    mlflow.log_metric("mae", mae)
-    mlflow.sklearn.log_model(model, "model")
+    mlflow.sklearn.log_model(model, artifact_path="model")
 
-    print("MAE:", mae)
+    print(f"🎯 MAE: {mae}")
