60efea3d357fc07ed9c8cb56da427318ce38aefb
Naila Syaki <165618505+NailaSyaki@users.noreply.github.com>
2025-12-07 10:02:25 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,15 +4,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
-# Tracking lokal
 mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("gempa-rf-experiment")
+mlflow.set_experiment("gempa-experiment")
 
+# Load dataset dari MLProject folder
 df = pd.read_csv("katalog-gempa_preprocessing.csv")
 
 X = df.drop(columns=["mag"])
 y = df["mag"]
 
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
@@ -24,3 +25,4 @@ with mlflow.start_run():
     model.fit(X_train, y_train)
 
     mlflow.sklearn.log_model(model, artifact_path="model")
+    print("Model training done, MLflow artifacts logged.")
